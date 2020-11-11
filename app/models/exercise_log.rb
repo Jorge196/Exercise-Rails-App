49aa7127a67308ec181320d14fdc7750bbda1a6f
Jorge04 <Jorge196@users.noreply.github.com>
2020-11-10 20:29:59 -0500

    custom validation method for exercise_log model

--- app/models/exercise_log.rb
+++ app/models/exercise_log.rb
@@ -2,12 +2,12 @@ class ExerciseLog < ApplicationRecord
   belongs_to :user
   belongs_to :exercise
   validates :user_id, :exercise_id 
-  validate :reps_weights_duration_and_notes
+  validate :reps_weight_duration_and_notes
 
 
-  def reps_weights_duration_and_notes 
-    if reps.present? 
-    
-
+  def reps_weight_duration_and_notes
+    unless (reps.present? && weight.present?) || duration.present? || notes.present? 
+      errors.add("Workout must contain either 'reps and weight, duration, or notes'")
+    end
   end 
 end
