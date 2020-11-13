1e75f89deb58dd138763a98c5f928640fd829705
Jorge04 <Jorge196@users.noreply.github.com>
2020-11-13 18:52:54 -0500

    migrations fully recovered and devise genarator has been created for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable, :validatable
     has_many :exercise_logs 
-    has_many :exercises, through: :exercise_logs 
+    has_many :exercises, -> { distinct }, through: :exercise_logs 
     validates :email, presence: true, uniqueness: true
     validates :user_name, presence: true  
 end
