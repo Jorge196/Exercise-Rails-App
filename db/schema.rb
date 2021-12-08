8e3ba7c507edb3ee00b01c50aaf8a8360975338a
Jorge196 <JoRamirez96@gmail.com>
2021-12-07 22:54:44 -0500

    trying to add postgresql server, but keep getting relation user doesn't exit error

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_191254) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "exercise_logs", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "exercise_id", null: false
-    t.integer "reps"
-    t.integer "weight"
-    t.integer "duration"
-    t.string "notes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["exercise_id"], name: "index_exercise_logs_on_exercise_id"
-    t.index ["user_id"], name: "index_exercise_logs_on_user_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "exercises", force: :cascade do |t|
-    t.string "name"
-    t.integer "exercise_log_count"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "user_name"
-    t.string "full_name"
-    t.string "avatar_url"
-    t.string "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "exercise_logs", "exercises"
-  add_foreign_key "exercise_logs", "users"
 end
